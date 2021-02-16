712911578ee79ca5a293b441b07c9a3b6afe9aed
RRoggia <renanzr@gmail.com>
2021-02-16 11:42:24 -0300

    add hide delegate

---
+++ chap7/my-refactor/hide-delegate-example01.js
@@ -0,0 +1,53 @@
+class Person {
+  constructor( name ) {
+    this._name = name
+  }
+
+  get name() {
+    return this._name
+  }
+
+  get manager() {
+    return this._department.manager
+  }
+
+  get department() {
+    return this._department
+  }
+
+  set department( department ) {
+    this._department = department
+  }
+}
+
+class Department {
+  constructor( chargeCode, manager ) {
+    this._chargeCode = chargeCode
+    this._manager = manager
+  }
+
+  get chargeCode() {
+    return this._chargeCode
+  }
+
+  set chargeCode( chargeCode ) {
+    this._chargeCode = chargeCode
+  }
+
+  get manager() {
+    return this._manager
+  }
+
+  set manager( manager ) {
+    this._manager = manager
+  }
+}
+
+const manager = new Person( 'Martin' )
+const employee = new Person( 'Fowler' )
+
+const department = new Department( '1112', manager )
+employee.department = department
+
+console.log( employee.manager )
+
